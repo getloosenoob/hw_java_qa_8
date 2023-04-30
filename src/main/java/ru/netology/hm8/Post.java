f242a39ec506bb149faf9d4fdadf92dd815c239c
getloosenoob <ka88@mail.ru>
2023-04-30 12:44:20 +0300

    Initial commit

---
+++ src/main/java/ru/netology/hm8/Post.java
@@ -0,0 +1,11 @@
+package ru.netology.hm8;
+
+public class Post {
+    FormDate birthday = new FormDate();
+    public String name;
+    public String passport;
+    public String patronymic;
+    public String phone;
+    public String surname;
+    public boolean subscription;
+}
